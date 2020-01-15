c25cff4467f46f58320e8c6e2503b3739040343f
axllow91 <dobremi91@gmail.com>
2020-01-15 09:48:08 +0200

    Implement Interfaces for services. Close #5.

---
+++ pet-clinic-data/src/main/java/com/mrn/sfgpetclinic/services/PetService.java
@@ -0,0 +1,11 @@
+package com.mrn.sfgpetclinic.services;
+
+import com.mrn.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/com/mrn/sfgpetclinic/services/VetService.java
@@ -0,0 +1,11 @@
+package com.mrn.sfgpetclinic.services;
+
+import com.mrn.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
