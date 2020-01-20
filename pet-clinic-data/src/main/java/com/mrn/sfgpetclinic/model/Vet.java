6b37e4ed21b4ff3ce2b071142972262f0222b50a
axllow91 <dobremi91@gmail.com>
2020-01-20 12:29:47 +0200

    Created Visit Entity. Closes #38

--- pet-clinic-data/src/main/java/com/mrn/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/mrn/sfgpetclinic/model/Vet.java
@@ -8,7 +8,7 @@ import java.util.Set;
 @Table(name = "vets")
 public class Vet extends Person {
 
-    @ManyToMany(fetch = FetchType.EAGER) // default of manytomany is lazy
+    @ManyToMany(fetch = FetchType.EAGER) // default of many to many is lazy
     @JoinTable(name="vet_specialities", joinColumns = @JoinColumn(name="vet_id"),
             inverseJoinColumns = @JoinColumn(name="speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
