35528dcd22cb7fd30f73f6a3136d6f36457b8432
Dobre <dobremi91@gmail.com>
2020-02-17 13:50:03 +0200

    Added create/update form and create/update visit form templates. Added builder pattern to petype. Closes #56

--- pet-clinic-web/src/main/java/com/mrn/sfgpetclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/com/mrn/sfgpetclinic/controllers/PetController.java
@@ -1,17 +1,19 @@
 package com.mrn.sfgpetclinic.controllers;
 
 import com.mrn.sfgpetclinic.model.Owner;
+import com.mrn.sfgpetclinic.model.Pet;
 import com.mrn.sfgpetclinic.model.PetType;
 import com.mrn.sfgpetclinic.services.OwnerService;
 import com.mrn.sfgpetclinic.services.PetService;
 import com.mrn.sfgpetclinic.services.PetTypeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Collection;
 
 
@@ -43,6 +45,31 @@ public class PetController {
     @ModelAttribute("owner")
     public Owner findOwner(@PathVariable("ownerId") Long ownerId) {
         return ownerService.findById(ownerId);
+    }
+
+    @GetMapping("/pets/new")
+    public String initCreationForm(Owner owner, Model model) {
+        Pet pet = new Pet();
+        owner.getPets().add(pet);
+        model.addAttribute("pet", pet);
+        return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
+    }
+
+    @PostMapping("/pets/new")
+    public String processCreationForm(Owner owner, @Valid Pet pet, BindingResult result, Model model) {
+        if(StringUtils.hasLength(pet.getName())
+                && pet.isNew()
+                && owner.getPet(pet.getName(), true) != null) {
+            result.rejectValue("name", "duplicate", "already exists");
+        }
+        owner.getPets().add(pet);
+        if(result.hasErrors()) {
+            model.addAttribute("pet", pet);
+            return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
+        } else {
+            petService.save(pet);
+            return "redirect:/owners/{ownerId}";
+        }
     }
 
 
