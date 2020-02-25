dba64f91c2e1ffa331a45a93a08e69c3323fa8c0
Dobre <dobremi91@gmail.com>
2020-02-25 10:18:26 +0200

    Implement Vets JSON Endpoint. Closes #63

--- pet-clinic-web/src/main/java/com/mrn/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/mrn/sfgpetclinic/controllers/VetController.java
@@ -1,9 +1,13 @@
 package com.mrn.sfgpetclinic.controllers;
 
+import com.mrn.sfgpetclinic.model.Vet;
 import com.mrn.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @RequestMapping()
 @Controller
@@ -19,5 +23,11 @@ public class VetController {
     public String list(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index"; // correspond with the thymeleaf html file from templates/vets/index.html
+    }
+
+
+    @RequestMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJSON() {
+        return vetService.findAll();
     }
 }
